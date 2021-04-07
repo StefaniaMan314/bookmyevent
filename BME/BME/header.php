26c432d82b50c5d1c40a3e88b974b52ceb606b6b
WHQ_NT_DOMAIN\AT063337 <alin.tintesan@cerner.com>
2021-04-07 13:23:39 +0300

    add bootstrap to header

--- BME/BME/header.php
+++ BME/BME/header.php
@@ -90,6 +90,8 @@ $check12=$link->query("SELECT `role` FROM `Associate` WHERE id='$assoc_id'");
     <script src="plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
     <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script>
         function display_c() {
             var refresh = 1000; // Refresh rate in milli seconds
